e90e61e100b92cfb2370d683913fa38588d1549c
Alina <alinadiac91@gmail.com>
2025-10-16 19:01:44 +0100

    Update main.js

--- Basic JavaScript Projects/Project2_functions/JS/main.js
+++ Basic JavaScript Projects/Project2_functions/JS/main.js
@@ -1,4 +1,11 @@
-function displayMessage() {
-    var message = "Hello, World!";
-    document.getElementById("output").innerHTML = message;
+// A function that uses the += operator
+function myFunction() {
+    // Create a variable with an initial value
+    var sentence = "Hello";
+    
+    // Use the += operator to add to the variable
+    sentence += ", World!";
+    
+    // Display the final sentence in the paragraph element with id 'Concatenate'
+    document.getElementById("Concatenate").innerHTML = sentence;
 }
