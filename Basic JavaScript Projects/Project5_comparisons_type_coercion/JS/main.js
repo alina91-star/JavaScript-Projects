4b4afd49c0144ee453909058496413f6e108dad6
Alina <alinadiac91@gmail.com>
2025-10-16 21:17:35 +0100

    Added Project5_comparisons_type_coercion and updated previous projects

---
+++ Basic JavaScript Projects/Project5_comparisons_type_coercion/JS/main.js
@@ -0,0 +1,5 @@
+// Declare a variable
+var favoriteColor = "Blue";
+
+// Use typeof operator to check the data type of the variable
+document.write("The type of 'favoriteColor' is: " + typeof favoriteColor);
