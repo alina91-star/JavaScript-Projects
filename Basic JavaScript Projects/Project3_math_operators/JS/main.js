5e30d40e8d1bb6827bee26cb43311f3cd256c85a
Alina <alinadiac91@gmail.com>
2025-10-16 20:24:49 +0100

    Create main.js

---
+++ Basic JavaScript Projects/Project3_math_operators/JS/main.js
@@ -0,0 +1,7 @@
+// A function that performs an addition operation
+function additionFunction() {
+    // Perform addition and store the result
+    var result = 5 + 7;
+    // Display the result in the paragraph element with id "Math"
+    document.getElementById("Math").innerHTML = "5 + 7 = " + result;
+}
